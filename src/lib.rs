f33f8763bdf98c9237f9d0481448eae1e68de281
Erik-Jan van de Wal <erikfourofour@proton.me>
2024-03-27 00:06:47 +0100

    fixed problem where hourblocks could be less than an hour

--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,8 @@ pub struct PriceData {
 pub struct RegressionData {
     pub price: f64,
     pub price_small: f64,
-    pub begin_timestamp: f64,
-    pub timestamp: f64,
+    pub begin_timestamp: u64,
+    pub timestamp: u64,
     pub timestamp_small: f64,
     pub time_value: f64,
     pub time_square: f64,
@@ -30,8 +30,8 @@ pub struct RegressionData {
 struct RegressionDataTemporary {
     pub price: f64,
     pub price_small: f64,
-    pub begin_timestamp: f64,
-    pub timestamp: f64,
+    pub begin_timestamp: u64,
+    pub timestamp: u64,
     pub timestamp_small: f64,
     pub time_value: f64,
     pub time_square: f64,
@@ -88,8 +88,14 @@ pub fn calculate_initial_regression(price_dataset: Vec<PriceData>) -> Vec<Regres
     dataset
 }
 // Function to update the previously calculated regression dataset with the newly arrived price data
-pub fn update_regression_dataset(mut regression_dataset: Vec<RegressionData>, price_dataset: Vec<PriceData>, regression_length_in_seconds: f64) -> Vec<RegressionData>{
+pub fn update_regression_dataset(mut regression_dataset: Vec<RegressionData>, price_dataset: Vec<PriceData>, regression_length: u64) -> Vec<RegressionData>{
+    if regression_length < 3600000 {
+        regression_dataset = check_regression_dataset_length(regression_dataset, regression_length);
+    }
+
     let temp_dataset: Vec<RegressionDataTemporary> = make_basic_regression_dataset(price_dataset, Some(regression_dataset[regression_dataset.len() - 1].begin_timestamp));
+    // Create a temp vec to hold items that should be deleted but because we dont have enough data are going to be appended to the result
+    let mut temp_vec: Vec<RegressionData> = Vec::new();
 
     for i in 0..temp_dataset.len() {
         let mut sum_time: f64 = regression_dataset[regression_dataset.len() - 1].sum_time + temp_dataset[i].timestamp_small;
@@ -98,13 +104,13 @@ pub fn update_regression_dataset(mut regression_dataset: Vec<RegressionData>, pr
         let mut sum_time_square: f64 = regression_dataset[regression_dataset.len() - 1].sum_time_square + temp_dataset[i].time_square;
 
         // The timestamp when the current regression data set begins is retrieved
-        let mut current_begin_timestamp: f64 = temp_dataset[0].begin_timestamp;
+        let mut current_begin_timestamp: u64 = temp_dataset[0].begin_timestamp;
         // An empty vector to store the indices of entries to be deleted is created
         let mut entry_to_be_deleted: Vec<usize> = Vec::new();
         // Iterating over each entry in the regression data set along with its index
         for (usize, entry) in regression_dataset.iter().enumerate() {
             // If the timestamp difference is greater than the specified regression length in seconds...
-            if temp_dataset[i].timestamp - entry.timestamp > regression_length_in_seconds {
+            if temp_dataset[i].timestamp - entry.timestamp > regression_length {
                 // Removed entry's time-related data from their respective sums
                 sum_time -= entry.timestamp_small;
                 sum_price_small -= entry.price_small;
@@ -119,10 +125,17 @@ pub fn update_regression_dataset(mut regression_dataset: Vec<RegressionData>, pr
             }
         }
 
+        // Calculate current hour identifier in millis
+        let curr_hour_identifier = temp_dataset[i].timestamp - (temp_dataset[i].timestamp % 3600000);
         // Sort in descending order to avoid issues with shifting elements and remove the elements from the Vec
         entry_to_be_deleted.sort_by(|a, b| b.cmp(a));
         for &index in &entry_to_be_deleted {
             if index < regression_dataset.len() {
+                let hour_identifier = regression_dataset[index].timestamp - (regression_dataset[index].timestamp % 3600000);
+                if curr_hour_identifier == hour_identifier {
+                    // Store this in the temp vec to potentially add back to result
+                    temp_vec.push(regression_dataset[index].clone());
+                }
                 regression_dataset.remove(index);
             } else {
                 println!("Index {} out of bounds", index);
@@ -135,14 +148,14 @@ pub fn update_regression_dataset(mut regression_dataset: Vec<RegressionData>, pr
         let regression_a_abs: f64 = calculate_regression_a_abs(regression_a, regression_b, temp_dataset[i].begin_timestamp);
 
         // Check if the timestamp minus half the regression length is less than the current timestamp
-        let check_timestamp: f64 = temp_dataset[i].timestamp - (0.5 * regression_length_in_seconds);
+        let check_timestamp: u64 = temp_dataset[i].timestamp - (0.5 * regression_length as f64) as u64;
         // Set the initial value of regression_b_half to a defined fake number
         let mut regression_b_half: f64 = 123456789.12;
         // Loop through entries in the regression_dataset, each entry's regression_b is assigned to regression_b_half
         // The loop breaks when an entry's timestamp is greater than the check_timestamp
         for entry in regression_dataset.iter() {
             regression_b_half = entry.regression_b;
-            if entry.timestamp > check_timestamp {
+            if entry.timestamp > check_timestamp as u64 {
                 //break the loop if the condition is met
                 break;
             }
@@ -176,22 +189,29 @@ pub fn update_regression_dataset(mut regression_dataset: Vec<RegressionData>, pr
         )
     }
 
+    // Because it can happen that an hour is not complete we need to append the old data back to create a full dataset
+    if temp_vec.len() > 0 {
+        temp_vec.sort_by(|a,b| a.timestamp.cmp(&b.timestamp));
+        temp_vec.extend(regression_dataset);
+        return temp_vec;
+    }
+
     regression_dataset
 }
 
 // Function to construct the basic regression dataset
-fn make_basic_regression_dataset(price_dataset: Vec<PriceData>, begin_timestamp: Option<f64>) -> Vec<RegressionDataTemporary> {
+fn make_basic_regression_dataset(price_dataset: Vec<PriceData>, begin_timestamp: Option<u64>) -> Vec<RegressionDataTemporary> {
     let mut result: Vec<RegressionDataTemporary> = Vec::new();
-    let mut begin_timestamp: f64 = begin_timestamp.unwrap_or(-0.1);
+    let mut begin_timestamp: u64 = begin_timestamp.unwrap_or(0);
 
     for price_data in price_dataset {
         let price: f64 = price_data.price;
-        let timestamp: f64 = price_data.timestamp as f64;
-        if begin_timestamp == -0.1 {
+        let timestamp: u64 = price_data.timestamp;
+        if begin_timestamp == 0 {
             begin_timestamp = timestamp;
         }
         let price_small: f64 = price / 1000.0;
-        let timestamp_small: f64 = (timestamp - begin_timestamp) / 100000.0;
+        let timestamp_small: f64 = (timestamp as f64 - begin_timestamp as f64) / 100000.0;
         let time_value: f64 = timestamp_small * price_small;
         let time_square: f64 = f64::powf(timestamp_small, 2.0);
         result.push(RegressionDataTemporary {
@@ -206,6 +226,17 @@ fn make_basic_regression_dataset(price_dataset: Vec<PriceData>, begin_timestamp:
     }
 
     result
+}
+
+fn check_regression_dataset_length(regression_dataset: Vec<RegressionData>, regression_length: u64) -> Vec<RegressionData> {
+    let mut new_dataset: Vec<RegressionData> = Vec::new();
+    let check_timestamp = regression_dataset[regression_dataset.len()-1].timestamp;
+    for regression_data in regression_dataset {
+        if check_timestamp - regression_data.timestamp < regression_length {
+            new_dataset.push(regression_data);
+        }
+    }
+    new_dataset
 }
 
 // Functions to calculate individual regression parameters
@@ -225,8 +256,8 @@ fn calculate_regression_value(regression_a: f64, regression_b: f64, time_stamp: 
     regression_a + ((regression_b * time_stamp) * 100000.0)
 }
 
-fn calculate_regression_a_abs(regression_a: f64, regression_b: f64, begin_timestamp: f64) -> f64 {
-    regression_a - ((begin_timestamp / 100000.0) * regression_b)
+fn calculate_regression_a_abs(regression_a: f64, regression_b: f64, begin_timestamp: u64) -> f64 {
+    regression_a - ((begin_timestamp as f64 / 100000.0) * regression_b)
 }
 
 fn calculate_initial_regression_b_half(regression_b: f64) -> f64 {
